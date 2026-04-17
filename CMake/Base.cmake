fae07957d0c1ab8d9332ee685bd0205e0223a43a
madelmann <madelmann@users.noreply.github.com>
2026-04-17 18:13:50 +0200

    switched on CMAKE_EXPORT_COMPILE_COMMANDS

--- CMake/Base.cmake
+++ CMake/Base.cmake
@@ -6,43 +6,43 @@ SET(CMAKE_CXX_FLAGS "-pedantic -fPIC -Wall -Werror -Wextra -Wunused -Wno-long-lo
 
 if( "${BUILD}" STREQUAL "" OR "${BUILD}" MATCHES "Debug")
 
-    # by default we build debug!
-    # MESSAGE("Setting default build to: Debug")
-    SET(CMAKE_BUILD_TYPE "Debug")
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-variable -Wno-unused-value")
+  # by default we build debug!
+  # MESSAGE("Setting default build to: Debug")
+  SET(CMAKE_BUILD_TYPE "Debug")
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-variable -Wno-unused-value")
 
-    # do more logging
-    add_definitions(-DDEBUG)
+  # do more logging
+  add_definitions(-DDEBUG)
 
-    # suppress "veraltete Konvertierung von Zeichenkettenkonstante in »char*« [-Werror=write-strings]"
-    add_definitions(-Wno-write-strings -Wno-unused-variable)
+  # suppress "veraltete Konvertierung von Zeichenkettenkonstante in »char*« [-Werror=write-strings]"
+  add_definitions(-Wno-write-strings -Wno-unused-variable)
 
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra -g")
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra -g")
 
 elseif( "${BUILD}" MATCHES "Release")
 
-    # MESSAGE("Setting default build to: Release")
-    SET(CMAKE_BUILD_TYPE "Release")
+  # MESSAGE("Setting default build to: Release")
+  SET(CMAKE_BUILD_TYPE "Release")
 
-    # suppress "veraltete Konvertierung von Zeichenkettenkonstante in »char*« [-Werror=write-strings]"
-    add_definitions(-Wno-write-strings -Wno-unused-variable -Wno-unused-result -Wno-unused-value)
+  # suppress "veraltete Konvertierung von Zeichenkettenkonstante in »char*« [-Werror=write-strings]"
+  add_definitions(-Wno-write-strings -Wno-unused-variable -Wno-unused-result -Wno-unused-value)
 
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-aliasing -O4")
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-aliasing -O4")
 
 else()
 
-    MESSAGE(FATAL_ERROR "Specify a valid build type: cmake -DBUILD=Release|Debug")
+  MESSAGE(FATAL_ERROR "Specify a valid build type: cmake -DBUILD=Release|Debug")
 
 endif()
 
 
 # specify default includes for the project
-SET(CMAKE_INCLUDE_CURRENT_DIR ON)
-
-SET(CMAKE_SKIP_RPATH TRUE)
 SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+SET(CMAKE_EXPORT_COMPILE_COMMANDS ON) # generate compile_commands.json
+SET(CMAKE_INCLUDE_CURRENT_DIR ON)
 SET(CMAKE_INSTALL_RPATH "")
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
+SET(CMAKE_SKIP_RPATH TRUE)
 
 # additional definitions
 #add_definitions(-DLOG_LEVEL=3)
@@ -55,4 +55,3 @@ INCLUDE(${PROJECT_SOURCE_DIR}/CMake/Builder.cmake)
 
 # include custom dependencies
 INCLUDE(${PROJECT_SOURCE_DIR}/CMake/Dependencies.cmake)
-
